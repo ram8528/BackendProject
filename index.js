53178b18c9dd434441df0ad581092866584e6143
ram8528 <106538883+ram8528@users.noreply.github.com>
2024-11-06 20:03:52 +0530

    FIrst time

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+
+
+const express = require('express')   // common js
+// import express from "express"   // module js both are same based on coder
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => {    //  / -> home route  agar request aa rha to response send kr do
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req,res)=>{
+    res.send('ramdeepkesharwani')
+})
+
+app.get('/login', (req,res) => {
+    res.send(`<h1>Please login at chai aur code </h1>`)
+})
+
+app.get('/youtube', (req,res)=>{
+    res.send(`<h2>Chai aur Code</h2>`)
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
